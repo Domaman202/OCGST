843f321a45efc0a1c6d5ebf513e3ebb5340b4a56
DomamaN202 <vip.domaman@mail.ru>
2023-07-03 17:38:29 +0700

    Clean

--- src/main/groovy/ru/DmN/ocgst/util/Utils.groovy
+++ src/main/groovy/ru/DmN/ocgst/util/Utils.groovy
@@ -11,19 +11,4 @@ class Utils {
     static byte[] itb(int i) {
         return new byte[]{(byte) (i >>> 24), (byte) (i >>> 16), (byte) (i >>> 8), (byte) i};
     }
-
-    static <T> T[][] split(T[] array, int size) {
-        var count = Math.ceil(array.length / size as int) as int
-        var elements = new Object[count][size]
-        var k = 0
-        for (i in 0..<count) {
-            var element = elements[i]
-            for (j in 0..<size) {
-                if (k == array.length)
-                    break
-                element[j] = array[k++]
-            }
-        }
-        return elements as T[][]
-    }
 }
